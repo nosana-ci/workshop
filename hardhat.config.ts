310329b26d9b5c5c6c9e57e310a0661be4db1060
djmbritt <david@effect.ai>
2023-05-17 09:42:44 +0200

    change branch

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -3,6 +3,12 @@ import "@nomicfoundation/hardhat-toolbox";
 
 const config: HardhatUserConfig = {
   solidity: "0.8.18",
-};
+  networks: {
+    polygon_mumbai: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY || '0'.repeat(64)]
+    }
+  }
+}
 
 export default config;
